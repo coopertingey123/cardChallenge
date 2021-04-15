5265bb8d2c33e1631c6b4f56d69f6a473ab8a38c
Cooper Tingey <cooper.tingey@gmail.com>
2021-04-14 22:55:16 -0700

    new functions

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,43 @@
-suit = ["Spades", "Clubs", "Diamonds", "Hearts"]
-value = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+class Cards():
+
+    def __init__(self, suit, value, cards)
+        self.suit = suit
+        self.value = value
+        self.cards = cards
+
+    suit = ["Spades", "Hearts", "Clubs", "Diamonds"]
+    value = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+    cards = ['2 of Spades', '3 of Spades', '4 of Spades', '5 of Spades', '6 of Spades', '7 of Spades', '8 of Spades', '9 of Spades', '10 of Spades', 'Jack of Spades', 'Queen of Spades', 'King of Spades', 'Ace of Spades', '2 of Hearts', '3 of Hearts', '4 of Hearts', '5 of Hearts', '6 of Hearts', '7 of Hearts', '8 of Hearts', '9 of Hearts', '10 of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts', 'Ace of Hearts', '2 of Clubs', '3 of Clubs', '4 of Clubs', '5 of Clubs', '6 of Clubs', '7 of Clubs', '8 of Clubs', '9 of Clubs', '10 of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs', 'Ace of Clubs', '2 of Diamonds', '3 of Diamonds', '4 of Diamonds', '5 of Diamonds', '6 of Diamonds', '7 of Diamonds', '8 of Diamonds', '9 of Diamonds', '10 of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds', 'Ace of Diamonds']
+    dealtCards = []
+    #deal default cards
+    # def defaultCards(suit, value):
+    #     for s in suit:
+    #         for v in value:
+    #             cards.append(f"{v} of {s}")
+    # defaultCards(suit, value)
+    # print(cards)
 
 
-cards = []
-#deal default cards
-def defaultCards(suit, value):
-    for s in suit:
-        for v in value:
-            cards.append(f"{v} of {s}")
-defaultCards(suit, value)
-print(cards)
+    #discard card off top of deck
+    def discardCard():
+        dealtCard = cards.pop(0)
+        return dealtCards.append(dealtCard)
 
 
-#deal card off top of deck
-def dealOneCard():
-  return print(cards[0])
+    #shuffle cards
+    import random
+    def shuffleCards():
+        random.shuffle(cards)
+        return cards
 
-#shuffle cards
-import random
-random.shuffle(cards)
-print(cards)
+    def cutDeck(cutLocation):
+        return (cards[cutLocation:] + cards[:cutLocation])
 
-#function to shuffle cards but returns "None"
-def shuffleCards(currentCards):
-  return print(random.shuffle(currentCards))
+    def orderCards(suit, value):
+        for s in suit:
+            for v in value:
+                cards.append(f"{v} of {s}")
+    
 
-shuffleCards(cards)
+    
 
